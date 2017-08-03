225d85ba2d0dfe01c4cd26c4f9a42dc5582fb7b0
davidjacob93 <davidjacob93@users.noreply.github.com>
2017-08-03 12:58:42 +0100

    #220 reopened issue due to unit test being wrong

--- src/Stepper/StepButton.spec.js
+++ src/Stepper/StepButton.spec.js
@@ -163,11 +163,10 @@ describe('<StepButton />', () => {
   describe('ID handling', () => {
     it('should use the supplied id without overriding', () => {
       const id = '12345';
-      const expectedId = `${id}-StepButton`;
       const wrapper = themedShallow(
         <StepButton id={id} />
       );
-      assert.strictEqual(wrapper.prop('id'), expectedId, 'should use provided id');
+      assert.strictEqual(wrapper.prop('id'), id, 'should use provided id');
     });
 
     it('should generate an id if one not supplied', () => {
