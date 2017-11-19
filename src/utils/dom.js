8cb7a5b441b25c8ef7123b92fcbf8edf1ea0d3fa
djbuckley <djbuckley@users.noreply.github.com>
2017-11-19 17:37:42 +0000

    added isInWindowOverlay to test if a node is on an element that covers the entire current window

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -19,4 +19,28 @@ export default {
     };
   },
 
+  /**
+   * Check if the given node is in an overlay covering the entire window.
+   * If the node is the body node, then it can't be an overlay.
+   * If the node is part of something with a size the same as the current window,
+   * then this is treated as being on an overlay.
+   */
+  isOnWindowOverlay(node) {
+    // short cut everything as the document body can't be an overlay for anything else
+    if (node === document.body) return false;
+
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+    let toTest = node;
+
+    while (toTest !== null) {
+      const nodeSize = toTest.getBoundingClientRect();
+      if ((nodeSize.height === windowHeight) && (nodeSize.width === windowWidth)) return true;
+      toTest = toTest.parentNode;
+      if (toTest === document.body) return false; // we may have back tracked all the way out to the body
+    }
+
+    return false;
+  },
+
 };
